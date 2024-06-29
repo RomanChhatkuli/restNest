54db56e29bd4213307e3041526e0c08402aada3a
Roman Chhatkuli <romanchhatkuli09@gmail.com>
2024-06-29 22:28:58 +0545

    navbar added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
-app.engine("ejs",ejsMate);
+app.engine("ejs",ejsMate); 
 
 // Connecting to database wanderlust in mongodb
 async function main() {
