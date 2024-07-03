dbd900a9fc32c678ce99dd976b0dfe88a5e08939
Roman Chhatkuli <romanchhatkuli09@gmail.com>
2024-07-03 18:48:21 +0545

    Changed default image error

--- Model/listing.js
+++ Model/listing.js
@@ -8,8 +8,8 @@ const listingSchema = new mongoose.Schema({
   description: String,
   image:  {
     type: String,
-    default: "https://unsplash.com/photos/exotic-waterfall-and-lake-landscape-of-plitvice-lakes-national-park-unesco-natural-world-heritage-and-famous-travel-destination-of-croatia-the-lakes-are-located-in-central-croatia-croatia-proper-zC-Led1p8wA",
-    set: (v) => v=== "" ? "https://unsplash.com/photos/exotic-waterfall-and-lake-landscape-of-plitvice-lakes-national-park-unesco-natural-world-heritage-and-famous-travel-destination-of-croatia-the-lakes-are-located-in-central-croatia-croatia-proper-zC-Led1p8wA":v
+    default: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) => v=== "" ? "https://images.unsplash.com/photo-1510798831971-661eb04b3739?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v
   },
   price: Number,
   location: String,
@@ -19,4 +19,3 @@ const listingSchema = new mongoose.Schema({
 const Listing = mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
-    
